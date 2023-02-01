f5c1d76aa1ba7c8660395f73768ce6cc75d15aeb
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2023-02-01 21:12:36 +0100

    restart project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-import { Givers, Home, Jins, Jonggivers, Kapoenen, Wouters, Add, Delete, NotFound } from "./pages";
-import { NavBar } from "./components";
+/** @format */
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/kapoenen' element={<Kapoenen />} />
-        <Route path='/wouters' element={<Wouters />} />
-        <Route path='/jonggivers' element={<Jonggivers />} />
-        <Route path='/givers' element={<Givers />} />
-        <Route path='/jins' element={<Jins />} />
-        <Route path='/add' element={<Add />} />
-        <Route path='/delete' element={<Delete />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -11,4 +13,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
